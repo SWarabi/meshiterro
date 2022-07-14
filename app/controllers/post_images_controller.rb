f72f459781266021429b8298a7eff07b81289495
EC2 Default User <ec2-user@ip-172-31-20-112.ap-northeast-1.compute.internal>
2022-07-14 02:24:55 +0000

    post_images

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,17 +1,36 @@
 class PostImagesController < ApplicationController
     def new
-        @post_image = PostImage.new
+      @post_image = PostImage.new
     end
     # 投稿データの保存
     def create
-        @post_image = PostImage.new(post_image_params)
-        @post_image.user_id = current_user.id 
-        @post_image.save
-        redirect_to post_images_path
+      @post_image = PostImage.new(post_image_params)
+      @post_image.user_id = current_user.id 
+      @post_image.save
+      redirect_to post_images_path
     end
     
-    private
+    def show
+      @post_image = PostImage.find(params[:id]) 
+      @post_comment = PostComment.new
+    end
+    
+    # アクション名
+    # @でindex.html.erbで使えるようになる
+    # @なしではファイル内のみでの使用
+    def index
+      @post_images = PostImage.all
+    end
+    
+    def destroy
+      @post_image = PostImage.find(params[:id])
+      @post_image.destroy
+      redirect_to '/post_images'
+    end
+
+   private
+   
     def post_image_params
-        params.require(:post_image).permit(:shop_name, :image, :caption)
+      params.require(:post_image).permit(:shop_name, :caption, :image)
     end
 end
